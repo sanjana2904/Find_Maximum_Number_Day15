2698117835d08acb8ce49b01e3e87678dc05bde4
sanjana k <sanjanarthi@gmail.com>
2021-03-22 21:37:29 +0530

    "Added UC4 - Printing Max method"

--- Find_Maximum_Number_Day15/Program.cs
+++ Find_Maximum_Number_Day15/Program.cs
@@ -7,8 +7,22 @@ namespace Find_Maximum_Number_Day15
         static void Main(string[] args)
         {
             Console.WriteLine("Testing the maximum of three numbers");
-            FindMaximumNum find = new FindMaximumNum();
-            Console.WriteLine(find.MaximumStringNumber("apple", "banana", "peach"));
+            int[] intArray = { 1, 2, 3 };
+            FindMaximumNum<int> find1 = new FindMaximumNum<int>(intArray);
+            float[] floatArray = { 11.1f, 12.2f, 13.3f };
+            FindMaximumNum<float> find2 = new FindMaximumNum<float>(floatArray);
+            string[] stringArray = { "apple", "banana", "peach" };
+            FindMaximumNum<string> find3 = new FindMaximumNum<string>(stringArray);
+            Console.WriteLine(find1.FindMaxValue());
+            Console.WriteLine(find2.FindMaxValue());
+            Console.WriteLine(find3.FindMaxValue());
+            //Console.WriteLine(find1.FindMaximumNum());
+            //Console.WriteLine(find2.FindMaximumNum());
+            //Console.WriteLine(find3.FindMaximumNum());
+
         }
     }
 }
+
+
+
